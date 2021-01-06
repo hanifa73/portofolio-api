3f645fa51f540e89eafa5b04a168fc23ae5fac26
hanifa73 <akkihanifa@gmail.com>
2021-01-06 08:35:18 +0300

    first commit

---
+++ config/plugins.js
@@ -0,0 +1,12 @@
+module.exports = ({ env }) => ({
+  
+  upload: {
+    provider: 'cloudinary',
+    providerOptions: {
+      cloud_name: 'ds5nfmz3q',
+      api_key: '949522699996476',
+      api_secret: '2RcW9TlO_6h00AboxWg3RiLVjmE',
+    },
+  },
+
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '8499fd52-b331-43a1-9e0a-7f0d4b7dd453'
+};
